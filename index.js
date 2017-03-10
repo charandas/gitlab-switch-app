b3ef91ac3cc82bf42f1549262d800ff95c25e9fd
Charandas Batra <charandas108@gmail.com>
2017-03-10 18:32:40 -0500

    Remove remnant of the view layer

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const digitalocean = require('./digitalocean');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (request, response) {
-  response.render('pages/index');
-});
-
 app.get('/up/:dropletName', function (req, res, next) {
   switch (req.params.dropletName) {
     case 'fathm-gitlab':
